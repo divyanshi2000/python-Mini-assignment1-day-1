210ae8bd39a6a166dae5ea57feefcfa352d24f56
divyanshi2000 <101087149+divyanshi2000@users.noreply.github.com>
2022-03-30 19:35:21 +0530

    Delete Lamda_assign.py

--- Lamda_assign.py
+++
@@ -1,6 +0,0 @@
-'''Write a Python program to find if a given string starts with a give character using Lambda.'''
-
-starts_with= lambda x: True if x.startswith('D') else False
-print (starts_with('Divyanshi'))
-starts_with = lambda x:True if x.startswith('D') else False
-print(starts_with('Rajput'))
